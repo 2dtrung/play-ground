7101de4fa1cc40cb508955bf31fa05f554c57a64
Duc Trung <sductrung@gmail.com>
2021-09-14 22:58:57 +0700

    add effect on hover btn

--- js/btn-hover.js
+++ js/btn-hover.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     $('.box-type').mouseover(function() {
         $(this).animate({marginTop: '-20px'},100)
     })
+
     $('.box-type').mouseleave(function() {
         $(this).animate({marginTop: '0px'},100)
     })
+
+    $('.header__bars').click(function() {
+        $('.header__menu').slideToggle().css({borderTop: '1px solid black', borderBottom: '1px solid black',height: '400px', display: 'flex', justifyContent: 'center', alignItems: 'flex-start', width: '100%'});
+    })
+
 })
